6416b05c113fc63b944b61ba1332e610dc562418
aymen <ameri.aymen@esprit.tn>
2023-07-16 19:06:31 +0200

    m3

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { F1Component } from './f1/f1.component';
+import { F2Component } from './f2/f2.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    F1Component
+    F1Component,
+    F2Component
   ],
   imports: [
     BrowserModule,
